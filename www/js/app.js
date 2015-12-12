98031520f02b46fce9de26fd85907f145fc35e04
sakmor <sakmor@gmail.com>
2015-12-12 15:11:44 +0800

    增加一堆功能
    1.增加讀取畫面
    2.增加彈跳動畫
    3.增加輸入錯誤的facebook帳號偵測(有BUG)
    4.增加一些美化效果

--- www/js/app.js
+++ www/js/app.js
@@ -1,8 +1,10 @@
 // This is a JavaScript file
-
 var facebookID;
+var facebookName;
+var facebooklikes;
 var ssid;
 var wifiPassword;
+var ssidTest;
 
 
 /**
@@ -12,27 +14,64 @@ function nextPage(url) {
 
 
     if (url) {
-        jQuery.getJSON('https://graph.facebook.com/?id=' + url + '&access_token=275419469142574%7CsvPvM8W-2HC09K9DArg59h5NPE4', 'likes', function (result) {
-            document.getElementById("facebookName").innerHTML = result.name;
+
+        jQuery.getJSON('https://graph.facebook.com/?id=' + url + '&access_token=275419469142574%7CsvPvM8W-2HC09K9DArg59h5NPE4', 'likes', function(result) {
+            document.getElementById("facebookName").innerHTML ='<i style="color:blue" class="fa fa-facebook-square"></i> '+ result.name;
             document.getElementById("facebookLikes").innerHTML = result.likes;
             $("#qrLogo").attr("src", "http://graph.facebook.com/" + result.username + "/picture?type=large");
             facebookID = result.id;
-
+            facebookName = result.name;
+            facebooklikes = result.likes;
         });
-        myNavigator.pushPage('page3.html');
+
+
+        myNavigator.pushPage('page3.html', {
+            onTransitionEnd: function() {
+                loadTest();
+            }
+        });
+
+
+
     } else {
 
-        document.getElementById("myText").innerHTML = "網址不可為空白";
+        document.getElementById("emptyText").innerHTML = "網址不可為空白";
     }
 
 
+}
+/**
+ *  當圖片讀取完畢的狀況下，隱藏Loading動畫，顯示內容
+ *  12/12新增判定: 若該facebook不是粉絲團，則顯示提示訊息。
+ */
+function loadTest() {
+    //如果facebookName不是undefined
+    if (facebookName) {
+        $("#qrLogo").load("http://graph.facebook.com/" + facebookName + "/picture?type=large", function() {
+            $('#page3').addClass('animated fadeInUpBig');
+            $("#page3").css('visibility', 'visible');
+            $("#loadIcon").css("display", "none");
+            $("#page3_bad").css("display", "none");
+
+        });
+    } else {
+    //如果facebookName是undefined
+            $("#loadIcon").css("display", "none");
+            $("#page3").css("display", "none");
+            $("#page3_bad").css('visibility', 'visible');
+            $('#page3_bad').addClass('animated shake');
+
+    }
+
+
+    console.log("loading complete" + facebookName);
 }
 
 /**
  *  第四頁點檢查使用者的SSID  
  */
 function blinker() {
-    console.log("偵測SSID中");
+
     getCurrentSSID();
     $('.blink_me').fadeOut(1000);
     $('.blink_me').fadeIn(1000);
@@ -45,6 +84,7 @@ function getCurrentSSID() {
 //如果連結WIFI成功
 function ssidHandler(s) {
     document.getElementById("yourSSID").innerHTML = s;
+
     //如果連結的SSID是 "FB_LIKE"
     if (s == "FB_LIKE06") {
         if (facebookID) {
@@ -55,6 +95,10 @@ function ssidHandler(s) {
     };
 }
 
+function fail() {
+    console.log("fail");
+
+}
 //設定機器
 function setup() {
     ssid = $('#SSID').val();
@@ -63,10 +107,10 @@ function setup() {
     $.ajax({
         url: "http://192.168.100.1:8080/Setting?SSID=" + ssid + "&PW=" + wifiPassword + "&FBID=" + facebookID,
         type: "GET",
-        success: function (response) {
+        success: function(response) {
             console.log(response); //預期回覆:想要回覆的訊息!!!
         },
-        error: function () {
+        error: function() {
             console.log("ajax error!");
         }
     });
@@ -76,16 +120,14 @@ function fail(e) {
     document.getElementById("connectID").innerHTML = "尚未連結WIFI";
 }
 
-//http://192.168.4.1/Setting?SSID=shome&PW=88888888&FBID=215976825086182
-//http://192.168.4.1/Setting
 function openlink() {
-    WifiWizard.getCurrentSSID(ssidHandler, fail);
 
     window.open("http://192.168.100.1:8080");
 
 
 }
 
+
+
 //todo:這行寫在這邊 會造成一開始就在偵測使用者的ssid
 //todo:偵測完使用者ssid後 應該要斷掉這個
-setInterval(blinker, 1000);
