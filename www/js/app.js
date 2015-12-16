eca4f9394119c92dd3f145c95ec904d9638f420c
sakmor <sakmor@gmail.com>
2015-12-16 10:57:54 +0800

    修正一些東西...
    1. 讓按紐文字顯示正常些
    2. 未輸入facebookID的顯示改為動畫

--- www/js/app.js
+++ www/js/app.js
@@ -5,40 +5,43 @@ var facebooklikes;
 var ssid;
 var wifiPassword;
 var ssidTest;
-var facebookID_recent=[];
+var facebookID_recent = [];
+var fromTag = false;
+
 
 //存檔系統
 //todo:整理、說明..還有ssid部份也要存檔
 //todo:還要清除...
-if(localStorage["facebookID_recent_storage"]){
+if (localStorage["facebookID_recent_storage"]) {
     facebookID_recent = JSON.parse(localStorage["facebookID_recent_storage"]);
 }
 
-var fromTag=false;
+
 
 /**
  *  顯示最近輸入的Facebook帳號
  *  todo:寫得有點奇怪..
  */
-function recentFacebook(){
+function recentFacebook() {
     localStorage["facebookID_recent_storage"] = JSON.stringify(facebookID_recent);
-    var i=facebookID_recent.length;
+    var i = facebookID_recent.length;
     $("#facebook_history").empty();
-    if(i>=1){
+    if (i >= 1) {
         i--;
-        for(i;i+1>0;i--){
-            $("#facebook_history").append($('<li><a onclick="tagFunction(\'' + i+ '\')" value=0 class="tag" href="#">'+facebookID_recent[i].facebookName+'</a></li>')); 
+        for (i; i + 1 > 0; i--) {
+            $("#facebook_history").append($('<li><a onclick="tagFunction(\'' + i + '\')" value=0 class="tag" href="#">' + facebookID_recent[i].facebookName + '</a></li>'));
         }
         // $('#facebook_history').addClass('animated bounceIn');
     }
 
 }
 
-function tagFunction(mytext){
+//如果使用者點擊tag標籤...
+function tagFunction(mytext) {
 
-     $('#facebookURL').val(facebookID_recent[mytext].facebookID);
-     nextPage(facebookID_recent[mytext].facebookID);
-     fromTag=true;
+    // $('#facebookURL').val(facebookID_recent[mytext].facebookID);
+    nextPage(facebookID_recent[mytext].facebookID);
+    fromTag = true;
 
 }
 
@@ -70,7 +73,12 @@ function nextPage(url) {
 
     } else {
 
-        document.getElementById("emptyText").innerHTML = "網址不可為空白";
+        //當輸入為空值時 將輸入框搖動
+        $('#facebookURL').removeClass('animated shake');
+        setTimeout(
+            function() {
+                $('#facebookURL').addClass('animated shake')
+            }, 1);
     }
 
 
@@ -87,10 +95,18 @@ function loadTest() {
             $("#page3").css('visibility', 'visible');
             $("#loadIcon").css("display", "none");
             $("#page3_bad").css("display", "none");
-            
+
             //如果不是透過點擊現有tag 加到最近輸入過的...
-            if(!fromTag){
-                 facebookID_recent.push({facebookName,facebookID});
+            if (!fromTag) {
+                //如果輸入超過20個..則移除第一項直到小於20
+                while (facebookID_recent.length >= 20) {
+                    facebookID_recent.shift();
+                }
+                //將最新的數值丟到最近輸入過的..
+                facebookID_recent.push({
+                    facebookName,
+                    facebookID
+                });
             }
         });
     } else {
@@ -179,7 +195,3 @@ $(document).on('swiperight', function() {
         }
     })
 });
-
-
-//todo:這行寫在這邊 會造成一開始就在偵測使用者的ssid
-//todo:偵測完使用者ssid後 應該要斷掉這個
